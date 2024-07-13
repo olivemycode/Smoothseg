6bbc7025fb21b3f330c4d6b2e591c1d2a7193cad
olivemycode <oli.li@alumni.unc.edu>
2024-07-13 00:56:47 -0400

    add intercept

--- R/qreg.R
+++ R/qreg.R
@@ -6,6 +6,7 @@
 #' @param x The predictor variable for quantile regression; this can be a vector of strings.
 #' @param y The response variable as a string for quantile regression.
 #' @param tau The quantile to be estimate (set to 0.5 by default)
+#' @param intercept Determines if there is an intercept in the model (set to TRUE by default).
 #' @return Returns a fitted model.
 #' @export
 #' @import forecast
@@ -15,9 +16,15 @@
 
 
 
-qreg <- function(df, x, y, tau = 0.5) {
+qreg <- function(df, x, y, tau = 0.5, intercept = TRUE) {
 
-  formula <- as.formula(paste(y, "~", paste(x, collapse = "+"))) # Create formula for quantile regression
+  if (intercept) {
+    formula <- as.formula(paste(response, "~", paste(predictors, collapse = "+")))
+  }
+
+  else {
+    formula <- as.formula(paste(response, "~", paste(predictors, collapse = "+"), "-1"))
+  }
 
   model <- rq(formula, data = df, tau = tau) # Create quantile regression model
 
